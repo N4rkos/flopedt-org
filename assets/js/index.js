574cfcd4fdde6d383855f61641c5dafbe33ba917
tgouvei <thomas.gouveia@etu.univ-tlse2.fr>
2020-03-09 17:22:03 +0100

    Static to symfony

---
+++ assets/js/index.js
@@ -0,0 +1,47 @@
+let navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function () {
+    addShadowToNavbar()
+});
+
+/**
+ * Anime la barre de navigation en lui rajoutant les ombres lorsque l'utilisateur défile.
+ */
+function addShadowToNavbar() {
+    let y = this.window.scrollY;
+    if (y == 0) {
+        navbar.classList.remove('shadow');
+    } else {
+        navbar.classList.add('shadow');
+    }
+}
+
+document.getElementById('menu-icon').addEventListener('click', function () {
+    document.getElementById('menu-icon').classList.toggle('change');
+    navbar.classList.toggle('change');
+
+});
+
+let swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+$('.flop__authors__pictures img').click(function() {
+    $('.flop__authors__pictures img').removeClass('active');
+    $(this).addClass('active');
+    $('.flop__authors__details').removeClass('active');
+    $('#' + $(this).attr('alt')).addClass('active');
+});
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DefaultController
+ * @package App\Controller
+ */
+class DefaultController extends AbstractController {
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index() {
+        return $this->render('default/index.html.twig');
+    }
+}
