890a2019035d1e73cbb27acc3472bd32ad07d268
tgouvei <thomas.gouveia@etu.univ-tlse2.fr>
2020-05-08 15:17:46 +0200

    Ajout axios + interface API

---
+++ src/backend/API.js
@@ -0,0 +1,23 @@
+
+const API_URL = "http://flopedt.org:1337"
+
+/**
+ * Interface entre le front et l'api strapi
+ */
+class API {
+
+    static getAsset(path) {
+        return `${API_URL}${path}`
+    }
+
+    //EXEMPLE DE FONCTION
+    // static async getTechnologies() {
+    //     const response = await Axios.get(`${API_URL}/technologies`)
+    //     if (response.status === 200) {
+    //         return response.data
+    //     }
+    //     return []
+    // }
+}
+
+export default API
