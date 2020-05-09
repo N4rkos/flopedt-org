41242e3d994679b0d323897fc95f57e5f1670084
tgouvei <thomas.gouveia@etu.univ-tlse2.fr>
2020-05-09 16:17:34 +0200

    Maj backend

--- src/backend/API.js
+++ src/backend/API.js
@@ -11,26 +11,17 @@ class API {
         return `${API_URL}${path}`
     }
 
-    //EXEMPLE DE FONCTION
-    // static async getTechnologies() {
-    //     const response = await Axios.get(`${API_URL}/technologies`)
-    //     if (response.status === 200) {
-    //         return response.data
-    //     }
-    //     return []
-    // }
-
     static async getClients() {
         const response = await Axios.get(`${API_URL}/clients`)
-        if (response.status === 200) {
-            return response.data
-        }
+        if (response.status === 200) return response.data
+        console.error('[ERREUR] : Impossible de récupérer les utilisateurs. Code HTTP : ' + response.status)
         return []
     }
 
     static async getSlides() {
         const response = await Axios.get(`${API_URL}/slideshows`)
         if (response.status === 200) return response.data
+        console.error('[ERREUR] : Impossible de récupérer les aperçus. Code HTTP : ' + response.status)
         return []
     }
 
