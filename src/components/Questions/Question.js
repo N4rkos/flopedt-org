d2e8386e86d3ff6235a1826f5d85cb095a426be3
tgouvei <thomas.gouveia@etu.univ-tlse2.fr>
2020-05-10 13:26:18 +0200

    Modifi FAQ

--- src/components/Questions/Question.js
+++ src/components/Questions/Question.js
@@ -9,12 +9,12 @@ const Question = ({ question }) => {
         <div class="card">
             <div class="card-header" id={`heading${q.id}`}>
                 <h2 class="mb-0">
-                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target={`#${uid}`} aria-expanded="true" aria-controls={q.id}>
+                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target={`#${uid}`}>
                         {q.question}
                     </button>
                 </h2>
             </div>
-            <div id={`${uid}`} class="collapse" aria-labelledby={`heading${q.id}`} data-parent="#accordionQuestions">
+            <div id={`${uid}`} class="collapse" data-parent="#accordionQuestions">
                 <div class="card-body">
                     <p>{q.response}</p>
                 </div>
