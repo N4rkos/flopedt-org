a3f9431345d70f4825bf4eebe87f6e20292b228c
tgouvei <thomas.gouveia@etu.univ-tlse2.fr>
2020-05-10 16:09:56 +0200

    Export des routes

---
+++ src/components/router.js
@@ -0,0 +1,9 @@
+/**
+ * Fichier contenant les routes du site.
+ */
+export const Routes = {
+    home: '/',
+    faq: '/foire-aux-questions',
+    contributors: '/contributeurs',
+    notFound: '*'
+}
